21b074c2fdad39010da3d21237615ab203729ebd
Ademar Costa <ademar_coosta@hotmail.com>
2023-01-03 00:14:04 -0300

    Exercicio de Desvio Condicional Aninhada

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,12 +1,17 @@
-var hora = 2
+/* Essa estrutura de desvio condicional aninhada onde tem um if dentro de outro if */
+
+var agora = new Date()
+var hora = agora.getHours() //estou pegando a hora atual com esse comando "getHors()"
 console.log(`São ${hora} horas`)
 
-if (hora < 12){
-    console.log('Bom Dia!')
-}else if (hora < 18){
+if (hora > 12 && hora < 18){
     console.log('Boa Tarde!')
-}else if(hora > 1 && hora < 5 ){
-    console.log('Boa Madruga!')
-}else{
+}else if (hora < 12 && hora > 5){
+    console.log('Bom Dia!')
+}else if(hora > 18 && hora <= 23){
     console.log('Boa Noite!')
+}else if(hora >= 0 && hora <= 5){ 
+    console.log('Boa Madrugada!')
+}else{
+    console.log('Horas INVALIDAS')
 }
