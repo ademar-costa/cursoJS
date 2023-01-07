1d809c67d335a7e16d3994370465fa44101b54e5
Ademar Costa <ademar_coosta@hotmail.com>
2023-01-07 02:26:45 -0300

    Estrutura de repetição, comando FOR

---
+++ aula14/ambiente.js
@@ -0,0 +1,9 @@
+/*O comando "for" tem a mesma função do comando "while", mas economizando mais linha de códigos.
+todo comando for tem um inciador, um teste lógico e o incremento. 
+tudo isso separados por ponto e virgula */
+
+console.log('vai começar...')
+for(var c = 1;c <= 6;c++){
+    console.log(c)
+}
+console.log('FIM!')
