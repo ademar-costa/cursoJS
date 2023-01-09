2bce057f9a140a3d67b04e199695a23ad106a794
Ademar Costa <ademar_coosta@hotmail.com>
2023-01-09 02:41:27 -0300

    Resolvido e corrigido e corrigido o ex16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,32 @@
-function calcular(){
-    var incio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
+function contar(){
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.getElementById('txtf')
+    var passo = window.document.getElementById('txtp')
     var res = window.document.getElementById('res')
 
-    var n1 = Number(incio.value)
-    var n2 = Number(fim.value)
-    var n3 = Number(passo.value)
+    if(ini.value == 0 || fim.value == 0){
+        alert('[ERRO] esta faltando dados!')
+        res.innerHTML = 'Impossivel Contar.'
+    }else{
+        res.innerHTML = 'Contando <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if(p == 0){
+            alert('Passo INVALIDO! (Considerando PASSO 1)')
+            p = 1
+        }
+        if(i < f){
+            for(var c = i;i <= f; i += p){
+                res.innerHTML += `${i} \u{1F449}`
+            }
+        }else{
+            for(var c = i;i >= f;i -= p){
+                res.innerHTML += `${i} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 
-    do{
-        res.innerHTML = n1
-        n1 = n1 + n3
-    }while(n1 <= n2)
+    
 }
