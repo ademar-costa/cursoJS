b6a67c5f4d25db2c5e0bdcdd0355669f9f2eb05d
Ademar Costa <ademar_coosta@hotmail.com>
2023-01-10 07:08:29 -0300

    Complementei um comentario no exercicio da aula 15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -2,7 +2,7 @@ var num = [5,8,9,3] //Criei um vetor(arry)
 num.push(1) //Adicionei o número "1" dentro do meu arry
 num.sort() //Coloquei em ordem crescente os número dentro do meu arry 
 console.log(num) //Mostrei no meu console todos os valores dentro do arry
-console.log(`O vetor tem ${num.length} posições`) // Com o comando "length" mostrei a quantidade de dados dentro do meu arry
+console.log(`O vetor tem ${num.length} posições`) // Com o comando "length" mostrei a quantidade de dados(indice) dentro do meu arry
 console.log(`O primeiro valor é ${num[0]}`) // E aqui eu acessei o valor na posição zero dentro do meu arry
 
 let pos = num.indexOf(9) // O comando "indexOf" mostra a posição de um dado no arry
