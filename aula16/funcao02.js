752c5a52b5382287950f9a75e9a6ab65c1aedad5
Ademar Costa <ademar_coosta@hotmail.com>
2023-01-10 08:00:04 -0300

    Estudos de funções

---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){// se os parametros não for passado, ele vai colocar o valor zero atribuido a ela dentro da function 
+    return n1 + n2
+}
+console.log(soma(7)) // aqui eu não declarei o segundo atributo só o primeiro 
+console.log(soma(2, 8))
